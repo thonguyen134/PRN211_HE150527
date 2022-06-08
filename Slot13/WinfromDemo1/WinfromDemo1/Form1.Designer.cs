3acaa37dba673d57060bd2a8083828d0f27e3760
tho dz <thondhe150527@fpt.edu.vn>
2022-06-08 23:35:15 +0700

    update slot 12 13
    winform slot 13

---
+++ Slot13/WinfromDemo1/WinfromDemo1/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace WinfromDemo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Cancel = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbDegree = new System.Windows.Forms.ComboBox();
+            this.rdGender = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.Save = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.txtPhone = new System.Windows.Forms.MaskedTextBox();
+            this.restart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Cancel
+            // 
+            this.Cancel.Location = new System.Drawing.Point(249, 338);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(94, 29);
+            this.Cancel.TabIndex = 0;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(324, 85);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(257, 27);
+            this.txtId.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(324, 131);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(257, 27);
+            this.txtName.TabIndex = 5;
+            this.txtName.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Text", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(249, 9);
+            this.label1.MaximumSize = new System.Drawing.Size(100, 100);
+            this.label1.MinimumSize = new System.Drawing.Size(300, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 50);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "EmployeeDetails";
+            this.label1.UseWaitCursor = true;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Gender:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(202, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Phone:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(202, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(202, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "ID:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(202, 266);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Degree:";
+            // 
+            // cbDegree
+            // 
+            this.cbDegree.FormattingEnabled = true;
+            this.cbDegree.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.cbDegree.Location = new System.Drawing.Point(324, 266);
+            this.cbDegree.Name = "cbDegree";
+            this.cbDegree.Size = new System.Drawing.Size(257, 28);
+            this.cbDegree.TabIndex = 12;
+            // 
+            // rdGender
+            // 
+            this.rdGender.AutoSize = true;
+            this.rdGender.Location = new System.Drawing.Point(324, 221);
+            this.rdGender.Name = "rdGender";
+            this.rdGender.Size = new System.Drawing.Size(63, 24);
+            this.rdGender.TabIndex = 13;
+            this.rdGender.TabStop = true;
+            this.rdGender.Text = "Male";
+            this.rdGender.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(455, 221);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(78, 24);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Female";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(487, 338);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(94, 29);
+            this.Save.TabIndex = 15;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(324, 180);
+            this.txtPhone.Mask = "(999) 000-0000";
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(257, 27);
+            this.txtPhone.TabIndex = 16;
+            this.txtPhone.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.txtPhone_MaskInputRejected);
+            // 
+            // restart
+            // 
+            this.restart.Location = new System.Drawing.Point(372, 338);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(94, 29);
+            this.restart.TabIndex = 17;
+            this.restart.Text = "Restart";
+            this.restart.UseVisualStyleBackColor = true;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.rdGender);
+            this.Controls.Add(this.cbDegree);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.Cancel);
+            this.Name = "Form1";
+            this.Text = "Employee Details";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbDegree;
+        private System.Windows.Forms.RadioButton rdGender;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button Save;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.MaskedTextBox txtPhone;
+        private System.Windows.Forms.Button restart;
+    }
+}
---
+++ Slot13/WinfromDemo1/WinfromDemo1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinfromDemo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Good bye bro!");
+            this.Close();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string id = txtId.Text;
+            string name = txtName.Text;
+            string gender = (rdGender.Checked ? "Female" : "Male");
+            string phone = txtPhone.Text;
+            string degree = cbDegree.Text;
+            StringBuilder buider = new StringBuilder();
+            buider.Append($"ID:{id}\n");
+            buider.Append($"name:{name}\n");
+            buider.Append($"gender:{gender}\n");
+            buider.Append($"phone:{phone}\n");
+            buider.Append($"degree:{degree}\n");
+            MessageBox.Show(buider.ToString());
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPhone_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPhone_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void restart_Click(object sender, EventArgs e)
+        {
+            txtId.ResetText();
+            txtName.ResetText();
+            txtPhone.ResetText();
+            cbDegree.ResetText();
+            
+        }
+    }
+}
